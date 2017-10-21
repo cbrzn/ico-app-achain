486af69e1795a7a1832b4432ffaa95c8a849b0bd
cbrzn <cesarbrazon10@gmail.com>
2017-10-20 22:22:47 -0400

    created TermAgreements model with fields

---
+++ app/views/term_agreements/_term_agreement.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! term_agreement, :id, :shareholder_address, :term_accepted, :terms_description, :created_at, :updated_at
+json.url term_agreement_url(term_agreement, format: :json)
---
+++ test/system/term_agreements_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class TermAgreementsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit term_agreements_url
+  #
+  #   assert_selector "h1", text: "TermAgreement"
+  # end
+end
