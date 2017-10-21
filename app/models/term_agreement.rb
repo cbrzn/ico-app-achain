441b42f8b76d4564cebcfbc59af0fc2b93f4b065
cbrzn <cesarbrazon10@gmail.com>
2017-10-21 00:17:49 -0400

    added TermAgreement validations

--- app/models/term_agreement.rb
+++ app/models/term_agreement.rb
@@ -1,4 +1,6 @@
 class TermAgreement < ApplicationRecord
-  validates :shareholder_address, presence: true
+  validates :shareholder_address, presence: true, length: { is: 42 },
+                                  format: { with: /[0]+[x]+[a-fA-F\d]*$/ },
+                                  uniqueness: true
   validates :term_accepted, presence: true
 end
